55db63b377a5ef83bf34a2b1dcdca541677e7cb6
Gabriel Birck <birckzin@gmail.com>
2025-12-10 19:12:23 -0300

    Adicionado cleanTransaction

--- src/main/java/com/gabrielbirck/desafio_itau/Controller/TransactionController.java
+++ src/main/java/com/gabrielbirck/desafio_itau/Controller/TransactionController.java
@@ -7,10 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 
@@ -29,6 +26,12 @@ public class TransactionController {
 
         transactionService.addTransaction(new Transaction(transaction.getValor(), transaction.getDataHora()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTransaction() {
+        transactionService.cleanTransactions();
+        return ResponseEntity.ok().build();
     }
 
 }
